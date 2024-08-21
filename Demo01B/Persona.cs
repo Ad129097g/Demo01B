7675140c05c0368f4c5f0eb54087fd3c6f2c3e06
Alumno <Alumno@LAB1507-24>
2024-08-20 19:51:17 -0500

    Agregar archivos de proyecto.

---
+++ Demo01B/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    public class Persona
+    {
+        public String Nombres { get; set; }
+        public String Apellidos { get; set;}
+        public int Edad { get; set; }
+        public string NombresCompletos { get; private set; }
+
+        public String MayorDeEdad()
+        {
+            string mensaje = "Es menor de edad";
+            if (Edad > 18)
+                mensaje = "Es mayor de edad";
+            return mensaje;
+        }
+
+        public void AsignarNombresCompletos()
+        {
+            NombresCompletos = Nombres + " " + Apellidos;
+        }
+
+
+
+    }
+}
---
+++ Demo01B/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {                                                                                                                                                           
+
+            Persona persona = new Persona();
+            persona.Nombres = "Nombre";
+            persona.Apellidos = "Apellidos";
+
+            Triangulo triangulo = new Triangulo();
+            triangulo.Altura = 1;
+            triangulo.Base = 2;
+            Console.WriteLine(triangulo.CalcularArea());
+
+            persona.AsignarNombresCompletos();
+
+            Persona persona1 = new Persona("Reyna", "Torrico");
+
+            Persona persona2 = new Persona
+            {
+                Nombres = "Derek",
+                Apellidos = "Torrico",
+                Edad = 5
+            };
+
+            Console.WriteLine(persona.NombresCompletos);
+            Console.WriteLine("Hola mundo");
+            Console.Read();
+        }
+    }
+}
